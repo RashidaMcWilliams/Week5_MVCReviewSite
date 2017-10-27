ec5c1b36096cc146ac630b8529ced784fe750205
McShi <rashida.mcwilliams@gmail.com>
2017-10-27 14:05:03 -0400

    Added Review class

---
+++ Week5_MVCReviewSite/Models/Review.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Week5_MVCReviewSite.Models
+{
+    public class Review
+    {
+        [Key]
+        public int ReviewID { get; set; }
+        public string UserName { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime PublishDate { get; set; }
+        public int Score { get; set; }
+
+
+    }
+}
