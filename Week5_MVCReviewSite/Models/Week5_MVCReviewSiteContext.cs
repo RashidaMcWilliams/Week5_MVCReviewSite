86e282e305296a05b37b4765aad184614e46c02a
McShi <rashida.mcwilliams@gmail.com>
2017-10-27 14:20:10 -0400

    Added Controllers

---
+++ Week5_MVCReviewSite/Models/Week5_MVCReviewSiteContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Week5_MVCReviewSite.Models
+{
+    public class Week5_MVCReviewSiteContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public Week5_MVCReviewSiteContext() : base("name=Week5_MVCReviewSiteContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<Week5_MVCReviewSite.Models.Review> Reviews { get; set; }
+
+        public System.Data.Entity.DbSet<Week5_MVCReviewSite.Models.Category> Categories { get; set; }
+    }
+}
